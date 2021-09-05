077cfb5f56438e1d7af5105805d3ce31a4d5d29b
TateM <TateMowbray@outlook.com>
2021-09-05 09:49:46 -0700

    playing with styles from bootswatch

--- src/Header.jsx
+++ src/Header.jsx
@@ -20,23 +20,27 @@ function Header(){
     return(
         <header >
             <Router>
-                <Navbar variant="dark" expand="lg">
-                    <Navbar.Brand><Link to="/"><img src={brandLogo} /></Link></Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link><Link to="/">Home</Link></Nav.Link>
-                        <Nav.Link><Link to="/about">about</Link></Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                        <NavDropdown.Item><Link to="/services">Services</Link></NavDropdown.Item>
-                        <NavDropdown.Item>Link 1</NavDropdown.Item>
-                        <NavDropdown.Item>Link 2</NavDropdown.Item>
-                        <NavDropdown.Divider />
-                        <NavDropdown.Item>Separated link</NavDropdown.Item>
+                <Navbar className="navbar navbar-expand-lg navbar-dark bg-primary" expand="lg" style={{padding:"10px"}}>
+                    <Navbar.Brand className="navbar-brand"><Link to="/"><img src={brandLogo} alt="AtmosHaire.com" /></Link></Navbar.Brand>
+                    <Navbar.Toggle className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation"/>
+                    <Navbar.Collapse className="collapse navbar-collapse" id="basic-navbar-nav">
+                    <Nav className="navbar-nav me-auto">
+                        <Nav.Link className="nav-item"><Link to="/" className="nav-link active">Home</Link></Nav.Link>
+                        <Nav.Link className="nav-item"><Link to="/about" className="nav-link">About</Link></Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown" className="nav-item dropdown" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">
+                            <NavDropdown.Item className="dropdown-item"><Link to="/services">Services</Link></NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item">Link 1</NavDropdown.Item>
+                            <NavDropdown.Item className="dropdown-item">Link 2</NavDropdown.Item>
+                            <NavDropdown.Divider className="dropdown-divider" />
+                            <NavDropdown.Item className="dropdown-item">Separated link</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link><Link to="/contact">Contact</Link></Nav.Link>
+                        <Nav.Link className="nav-item"><Link to="/contact" className="nav-link">Contact</Link></Nav.Link>
                     </Nav>
                     </Navbar.Collapse>
+                    <form className="d-flex">
+                            <input className="form-control me-sm-2" type="text" placeholder="Search" />
+                            <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    </form>
                 </Navbar>
                 <Switch>
                     <Route path="/about">
